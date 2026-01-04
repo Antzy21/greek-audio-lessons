e82e56f927cf6abe40d8c0bb614f355da4534c7b
= <=>
2026-01-04 20:59:31 +0000

    Add Lesson 1 audio

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup></script>
+<script setup>
+import AudioPlayer from './components/AudioPlayer.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Greek Audio Lessons</h1>
+  
+  <h2>Introduction</h2>
+  <AudioPlayer src="../assets/01 - Introductory Lesson.mp3" number="1" />
+
+  <h2>Lesson 1</h2>
+  <AudioPlayer src="../assets/02 - Lesson 1-1.mp3" number="2" />
+  <AudioPlayer src="../assets/03 - Lesson 1-2.mp3" number="3" />
+  <AudioPlayer src="../assets/04 - Lesson 1-3.mp3" number="4" />
+  <AudioPlayer src="../assets/05 - Lesson 1-4.mp3" number="5" />
+  <AudioPlayer src="../assets/06 - Lesson 1-5.mp3" number="6" />
+  <AudioPlayer src="../assets/07 - Lesson 1-6.mp3" number="7" />
+
+
 </template>
 
 <style scoped></style>
