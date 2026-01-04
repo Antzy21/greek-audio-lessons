853336a51e7aa59a55004981b834b391f407777e
= <=>
2026-01-04 21:17:35 +0000

    Move audio files

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,15 @@ import AudioPlayer from './components/AudioPlayer.vue'
   <h1>Greek Audio Lessons</h1>
   
   <h2>Introduction</h2>
-  <AudioPlayer src="../assets/01 - Introductory Lesson.mp3" number="1" />
+  <AudioPlayer src="../assets/01-IntroductoryLesson.mp3" number="1" />
 
   <h2>Lesson 1</h2>
-  <AudioPlayer src="../assets/02 - Lesson 1-1.mp3" number="2" />
-  <AudioPlayer src="../assets/03 - Lesson 1-2.mp3" number="3" />
-  <AudioPlayer src="../assets/04 - Lesson 1-3.mp3" number="4" />
-  <AudioPlayer src="../assets/05 - Lesson 1-4.mp3" number="5" />
-  <AudioPlayer src="../assets/06 - Lesson 1-5.mp3" number="6" />
-  <AudioPlayer src="../assets/07 - Lesson 1-6.mp3" number="7" />
+  <AudioPlayer src="../assets/02-Lesson1-1.mp3" number="2" />
+  <AudioPlayer src="../assets/03-Lesson1-2.mp3" number="3" />
+  <AudioPlayer src="../assets/04-Lesson1-3.mp3" number="4" />
+  <AudioPlayer src="../assets/05-Lesson1-4.mp3" number="5" />
+  <AudioPlayer src="../assets/06-Lesson1-5.mp3" number="6" />
+  <AudioPlayer src="../assets/07-Lesson1-6.mp3" number="7" />
 
 
 </template>
