6569c1173026c6cb4e3a7dd8aaebb81e70619068
= <=>
2026-01-05 18:23:02 +0000

    Add titles for Lesson 1

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,13 @@ import AudioPlayer from './components/AudioPlayer.vue'
   <AudioPlayer src="../assets/01-IntroductoryLesson.mp3" number="1" />
 
   <h2>Lesson 1</h2>
-  <AudioPlayer src="../assets/02-Lesson1-1.mp3" number="2" />
-  <AudioPlayer src="../assets/03-Lesson1-2.mp3" number="3" />
-  <AudioPlayer src="../assets/04-Lesson1-3.mp3" number="4" />
-  <AudioPlayer src="../assets/05-Lesson1-4.mp3" number="5" />
-  <AudioPlayer src="../assets/06-Lesson1-5.mp3" number="6" />
-  <AudioPlayer src="../assets/07-Lesson1-6.mp3" number="7" />
-
-
+  
+  <span>Γειά σας <AudioPlayer src="../assets/02-Lesson1-1.mp3" number="2" /></span>
+  <span>Πώς σας λένε; <AudioPlayer src="../assets/03-Lesson1-2.mp3" number="3" /></span>
+  <span>Γειά σου <AudioPlayer src="../assets/04-Lesson1-3.mp3" number="4" /></span>
+  <span>Πώς σε λένε; <AudioPlayer src="../assets/05-Lesson1-4.mp3" number="5" /></span>
+  <span>Από πού είστε; <AudioPlayer src="../assets/06-Lesson1-5.mp3" number="6" /></span>
+  <span>Από πού είσαι; <AudioPlayer src="../assets/07-Lesson1-6.mp3" number="7" /></span>
 </template>
 
 <style scoped></style>
